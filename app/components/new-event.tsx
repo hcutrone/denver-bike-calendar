5b990a951cf413c02f03bb9585aeb707b31de1c3
HCutrone <hcutrone@calpoly.edu>
2025-03-30 22:28:35 -0600

    New Events: Support description and restyle
    Allow users to add a description to new events, shown when the popover
    is opened when clicking on an event on the calendar.

--- app/components/new-event.tsx
+++ app/components/new-event.tsx
@@ -1,6 +1,13 @@
 "use client";
 
-import { Button, Dialog, Flex } from "@radix-ui/themes";
+import {
+  Box,
+  Button,
+  Dialog,
+  Flex,
+  TextArea,
+  TextField,
+} from "@radix-ui/themes";
 import { Label } from "radix-ui";
 import { useEffect, useState } from "react";
 import { Event } from "react-big-calendar";
@@ -25,45 +32,62 @@ export function NewEventDialog({
   useEffect(() => setNewEvent({ start, end }), [start, end]);
   return (
     <Dialog.Root open={isOpen}>
-      <Dialog.Content>
-        <Flex gap="4" direction="column">
+      <Dialog.Content maxWidth="600px">
+        <Flex gap="3" direction="column">
           <Dialog.Title>New Event</Dialog.Title>
-          <Flex justify="between" direction="row">
+          <Box>
             <Label.Root htmlFor="title">Title</Label.Root>
-            <input
+            <TextField.Root
               id="title"
+              size="3"
               value={newEvent?.title?.toString() ?? ""}
               onChange={(e) =>
                 setNewEvent({ ...newEvent, title: e.target.value })
               }
+              style={{ height: "44px" }}
             />
+          </Box>
+          <Flex gap="3" justify="between">
+            <Box>
+              <Label.Root htmlFor="start">Start</Label.Root>
+              <TextField.Root
+                id="start"
+                type="datetime-local"
+                value={newEvent.start?.toISOString().split(".")[0]}
+                onChange={(e) =>
+                  setNewEvent({
+                    ...newEvent,
+                    start: new Date(e.target.value),
+                  })
+                }
+                style={{ height: "44px" }}
+              />
+            </Box>
+            <Box>
+              <Label.Root htmlFor="end">End</Label.Root>
+              <TextField.Root
+                id="end"
+                type="datetime-local"
+                value={newEvent.end?.toISOString().split(".")[0]}
+                onChange={(e) =>
+                  setNewEvent({ ...newEvent, end: new Date(e.target.value) })
+                }
+                style={{ height: "44px" }}
+              />
+            </Box>
           </Flex>
-          <Flex justify="between" direction="row">
-            <Label.Root htmlFor="start">Start</Label.Root>
-            <input
-              id="start"
-              type="datetime-local"
-              value={newEvent.start?.toISOString().split(".")[0]}
+          <Box>
+            <Label.Root htmlFor="body">Description</Label.Root>
+            <TextArea
+              size="3"
+              resize="vertical"
+              value={newEvent.body ?? ""}
               onChange={(e) =>
-                setNewEvent({
-                  ...newEvent,
-                  start: new Date(e.target.value),
-                })
+                setNewEvent({ ...newEvent, body: e.target.value })
               }
             />
-          </Flex>
-          <Flex justify="between" direction="row">
-            <Label.Root htmlFor="end">End</Label.Root>
-            <input
-              id="end"
-              type="datetime-local"
-              value={newEvent.end?.toISOString().split(".")[0]}
-              onChange={(e) =>
-                setNewEvent({ ...newEvent, end: new Date(e.target.value) })
-              }
-            />
-          </Flex>
-          <Flex justify="end" gap="4">
+          </Box>
+          <Flex justify="end" gap="4" style={{ marginTop: "14px" }}>
             <Button
               disabled={!newEvent.start || !newEvent.end}
               onClick={() =>
