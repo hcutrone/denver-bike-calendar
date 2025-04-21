682bf24aff894338134af5f5bf009e0c94698b76
HCutrone <hcutrone@calpoly.edu>
2025-04-21 00:26:52 -0600

    Events API: Use environment variable for domain
    This lets us call the API from the server, i.e. on page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,23 +2,12 @@
 
 import { BikeCalendar } from "./components";
 import { CalendarContextProvider } from "./components/calendar-context";
-import { DBEvent, EventData } from "./types";
+import { selectEvents } from "./helpers/events";
 
 export default async function Home() {
-  const events = await fetch("http://localhost:3000/api/events", {
-    method: "GET",
-  });
-  const formattedEvents: EventData[] = JSON.parse(
-    (await events.json()).body
-  ).map((event: DBEvent) => {
-    return {
-      ...event,
-      start_time: new Date(event.start_time),
-      end_time: new Date(event.end_time),
-    };
-  });
+  const events = await selectEvents();
   return (
-    <CalendarContextProvider dbEvents={formattedEvents}>
+    <CalendarContextProvider dbEvents={events ?? []}>
       <BikeCalendar />
     </CalendarContextProvider>
   );
