2f87cd78e8267b4af65cb15c8b92dc984d17d664
Harrison Cutrone <harrison@ifixit.com>
2025-07-25 11:07:33 -0600

    Biome: Install and format

--- app/components/calendar.tsx
+++ app/components/calendar.tsx
@@ -1,46 +1,46 @@
 "use client";
 
+import { Box } from "@radix-ui/themes";
+import moment from "moment";
 import { Calendar, momentLocalizer } from "react-big-calendar";
 import withDragAndDrop, {
-  withDragAndDropProps,
+	type withDragAndDropProps,
 } from "react-big-calendar/lib/addons/dragAndDrop";
-import moment from "moment";
-import { Box } from "@radix-ui/themes";
 import { useCalendarContext } from "./calendar-context";
 
 const localizer = momentLocalizer(moment);
 const DnDCalendar = withDragAndDrop(Calendar);
 
 export function BikeCalendar() {
-  const { events, openEventDialog } = useCalendarContext();
+	const { events, openEventDialog } = useCalendarContext();
 
-  const onEventResize: withDragAndDropProps["onEventResize"] = (data) => {
-    console.log(data);
-  };
+	const onEventResize: withDragAndDropProps["onEventResize"] = (data) => {
+		console.log(data);
+	};
 
-  const onEventDrop: withDragAndDropProps["onEventDrop"] = (data) => {
-    console.log(data);
-  };
+	const onEventDrop: withDragAndDropProps["onEventDrop"] = (data) => {
+		console.log(data);
+	};
 
-  return (
-    <Box maxWidth="1250px" m="4" p="4" mx="auto">
-      <DnDCalendar
-        localizer={localizer}
-        events={events.map((event) => event.event)}
-        onEventDrop={onEventDrop}
-        onEventResize={onEventResize}
-        resizable
-        selectable
-        onSelectSlot={(event) => {
-          if (
-            document.getElementsByClassName("eventPopoverContent").length > 0
-          ) {
-            return;
-          }
-          openEventDialog(event);
-        }}
-        style={{ height: "80vh" }}
-      />
-    </Box>
-  );
+	return (
+		<Box maxWidth="1250px" m="4" p="4" mx="auto">
+			<DnDCalendar
+				localizer={localizer}
+				events={events.map((event) => event.event)}
+				onEventDrop={onEventDrop}
+				onEventResize={onEventResize}
+				resizable
+				selectable
+				onSelectSlot={(event) => {
+					if (
+						document.getElementsByClassName("eventPopoverContent").length > 0
+					) {
+						return;
+					}
+					openEventDialog(event);
+				}}
+				style={{ height: "80vh" }}
+			/>
+		</Box>
+	);
 }
