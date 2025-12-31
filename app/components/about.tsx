b059b07504dace63d3a9d80c44b519cd24f45716
Harrison Cutrone <harrison@ifixit.com>
2025-12-30 17:52:56 -0700

    About/General: Add
    We may end up merging these sections later, as they have similar content

---
+++ app/components/about.tsx
@@ -0,0 +1,59 @@
+import { Flex, Text } from "@radix-ui/themes";
+import Image from "next/image";
+
+export const About = () => (
+	<Flex
+		style={{
+			backgroundColor: "var(--lime-9)",
+		}}
+	>
+		<Flex
+			direction="column"
+			style={{
+				padding: "32px",
+				maxWidth: "1280px",
+				margin: "auto",
+				gap: "8px",
+			}}
+		>
+			<Text size={"9"}>What is Denver Bike Fest?</Text>
+			<Text>
+				What started as a simple idea from Emily Kleinfelter (Bike This City) on
+				a Thanksgiving social ride in 2024 hosted by Z Cycle turned into the
+				ultimate celebration of the city&apos;s vibrant bicycle community.
+				Denver Bike Fest is all about celebrating community, connection, and the
+				joy of getting around on two wheels. Hosted by Z Cycle Shop and Bike
+				This City, the festival brings together local community groups,
+				nonprofits, and businesses to showcase their efforts to get more people
+				on bikes. Featuring live music, food trucks, and beer all sourced
+				locally, Denver Bikes Fest is all about supporting and building
+				community. Our first-ever Denver Bike Fest on April 26, 2025 at City
+				Park Pavilion was a huge success — over 2,000 people came out to
+				connect, learn, and party!
+			</Text>
+
+			<Flex direction="row" style={{ marginTop: "32px", gap: "32px" }}>
+				<Image
+					src="/bicycle.png"
+					alt="Bike This City"
+					width={300}
+					height={100}
+				/>
+				<Flex direction="column" gap="8px">
+					<Text size="8">Bike This City</Text>
+					<Text style={{ lineHeight: "1.6" }}>
+						Bike This City is Emily Kleinfelter, a transportation planner, road
+						safety professional, and community organizer who believes bikes can
+						transform how we experience our community. She brought the idea for
+						Denver Bike Fest to Z Cycle Shop to team up and to bring it to life.
+						Through her platform, Emily helps connect people to Denver’s bike
+						community, shares real-life examples of how bikes are
+						transportation, and why safe streets matter for all of us. Bike This
+						City leads the organizing and outreach efforts behind Denver Bike
+						Fest.
+					</Text>
+				</Flex>
+			</Flex>
+		</Flex>
+	</Flex>
+);
---
+++ app/components/general.tsx
@@ -0,0 +1,93 @@
+import { Flex, Section, Text } from "@radix-ui/themes";
+import Image from "next/image";
+
+export const General = () => (
+	<Section>
+		<Flex
+			direction="column"
+			style={{
+				padding: "32px",
+				maxWidth: "1280px",
+				margin: "auto",
+			}}
+			id="about"
+			gap="16px"
+		>
+			<Text size="9" style={{ color: "var(--lime-11)" }}>
+				General Info
+			</Text>
+			<Flex direction="row" gap="32px" style={{ marginBottom: "32px" }}>
+				<Flex
+					direction="column"
+					style={{
+						backgroundColor: "var(--lime-9)",
+						borderRadius: "25px",
+						padding: "16px",
+						textWrap: "nowrap",
+						fontWeight: "bold",
+					}}
+				>
+					<Text>Date: June 13, 2026</Text>
+					<Text>Time: 3-9 PM</Text>
+					<Text>Location: York Street Yards</Text>
+					<Text>Cost: Free</Text>
+				</Flex>
+				<Text style={{ color: "var(--lime-11)" }}>
+					{`Denver Bike Fest is a community celebration of bikes, music, and
+      connection. The festival brings together local bike groups, shops,
+      artists, and food vendors to show how fun, creative, and inclusive
+      Denver’s cycling community can be. Whether you ride every day or just love
+      the vibe, you’re invited to join in!`}
+				</Text>
+			</Flex>
+			<Text size="8" style={{ color: "var(--lime-11)" }}>
+				What to Expect
+			</Text>
+			<Flex direction="row" gap="32px">
+				<Image
+					src="/bicycle.png"
+					alt="Bike This City"
+					width={200}
+					height={50}
+				/>
+				<Flex direction="column">
+					<Text style={{ color: "var(--lime-11)" }}>Community Connections</Text>
+					<Text style={{ color: "var(--lime-11)" }}>
+						Meet over 25 local bike organizations, community groups, and shops
+						that make Denver a better place to ride.
+					</Text>
+				</Flex>
+			</Flex>
+			<Flex direction="row" gap="32px">
+				<Image
+					src="/bicycle.png"
+					alt="Bike This City"
+					width={200}
+					height={50}
+				/>
+				<Flex direction="column">
+					<Text style={{ color: "var(--lime-11)" }}>Community Connections</Text>
+					<Text style={{ color: "var(--lime-11)" }}>
+						Meet over 25 local bike organizations, community groups, and shops
+						that make Denver a better place to ride.
+					</Text>
+				</Flex>
+			</Flex>
+			<Flex direction="row" gap="32px">
+				<Image
+					src="/bicycle.png"
+					alt="Bike This City"
+					width={200}
+					height={50}
+				/>
+				<Flex direction="column">
+					<Text style={{ color: "var(--lime-11)" }}>Community Connections</Text>
+					<Text style={{ color: "var(--lime-11)" }}>
+						Meet over 25 local bike organizations, community groups, and shops
+						that make Denver a better place to ride.
+					</Text>
+				</Flex>
+			</Flex>
+		</Flex>
+	</Section>
+);
