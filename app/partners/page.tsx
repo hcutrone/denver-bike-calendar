98d8a0cff0720d19302ba7a6d2ad7736b474a2bf
Harrison Cutrone <harrison@ifixit.com>
2026-03-05 18:20:19 -0700

    Partners: Add tier cards
    These will need to be tweaked to be responsive, but they look great on
    desktop!

--- app/partners/page.tsx
+++ app/partners/page.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-import { Flex, Heading, Text } from "@radix-ui/themes";
+import { Em, Flex, Heading, Text } from "@radix-ui/themes";
 import { CollapsiblePartnerGrid } from "@/components";
-import { partners } from "../../partner-data";
+import { partners, partnerTiers } from "../../partner-data";
 
 export default function PartnersPage() {
    return (
@@ -32,6 +32,7 @@ export default function PartnersPage() {
                "Denver Bike Fest is proud to partner with so many amazing local groups, organizations, and businesses."
             }
          </Text>
+         <PartnerTiers />
          {partners.map((partner) => (
             <CollapsiblePartnerGrid key={partner.header} partner={partner} />
          ))}
@@ -55,3 +56,91 @@ export default function PartnersPage() {
       </Flex>
    );
 }
+
+const PartnerTiers = () => (
+   <Flex direction="row" gap="16px" wrap="wrap" justify="between">
+      {partnerTiers.map((tier) => (
+         <PartnerInfoCard key={tier.name} title={tier.name} color={tier.color}>
+            <Flex
+               width="100%"
+               p="4px"
+               mt="42px"
+               justify="center"
+               style={{ backgroundColor: tier.color }}
+            >
+               <Text size="7" weight="bold" style={{ color: "white" }}>
+                  ${tier.price}
+               </Text>
+            </Flex>
+            <Flex direction="column" px="16px" pb="12px" gap="8px">
+               <ul style={{ listStyleType: "disc", paddingLeft: "16px" }}>
+                  {tier.bullets.map((bullet) => (
+                     <li key={bullet}>
+                        <Text style={{ color: "black" }}>{bullet}</Text>
+                     </li>
+                  ))}
+               </ul>
+               <Flex justify="center" width="100%">
+                  {tier.subtext && (
+                     <Text
+                        size="1"
+                        weight="light"
+                        style={{ color: "black", textAlign: "center" }}
+                     >
+                        <Em>{tier.subtext}</Em>
+                     </Text>
+                  )}
+               </Flex>
+            </Flex>
+         </PartnerInfoCard>
+      ))}
+   </Flex>
+);
+
+const PartnerInfoCard = ({
+   title,
+   color,
+   children,
+}: {
+   title: string;
+   color: string;
+   children: React.ReactNode;
+}) => (
+   <Flex
+      position="relative"
+      direction="column"
+      maxWidth="calc(25% - 12px)"
+      mt="16px"
+      gap="8px"
+      style={{
+         backgroundColor: "white",
+         borderRadius: "25px",
+         border: `3px solid ${color}`,
+      }}
+   >
+      <Flex
+         position="absolute"
+         top="0"
+         left="50%"
+         width="75%"
+         align="center"
+         justify="center"
+         style={{
+            backgroundColor: color,
+            padding: "4px 16px",
+            borderRadius: "25px",
+            transform: "translate(-50%, -50%)",
+            textAlign: "center",
+         }}
+      >
+         <Heading
+            as="h2"
+            size="5"
+            style={{ color: "white", width: "min-content" }}
+         >
+            {title}
+         </Heading>
+      </Flex>
+      {children}
+   </Flex>
+);
